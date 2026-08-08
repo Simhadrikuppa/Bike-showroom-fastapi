14f7a28453498ac2aa87fa7c241a2fd4a383a3a9
SimhadriKuppa <simhadrikuppa@gmail.com>
2026-08-08 15:28:34 +0530

    updated

--- database.py
+++ database.py
@@ -1,30 +1,35 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# MySQL Database URL
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/bike_showroom"
 
-# Create Engine
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
 engine = create_engine(
     DATABASE_URL,
     echo=True
 )
 
-# Create Session
+
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Base Class
+
 Base = declarative_base()
 
 
-# Dependency
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
